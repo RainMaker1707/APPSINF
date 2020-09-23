31bcb27f5d35bc8bc37ff6439544a7425eb44139
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2020-09-23 23:04:52 +0200

    first try for responsive  (fail)

--- scripts/navOnTop.js
+++ scripts/navOnTop.js
@@ -4,17 +4,28 @@ function elementPosition(a) {
         clientX: a.offsetLeft,
         clientY: a.offsetTop,
         viewportX: (b.x || b.left),
-        viewportY: (b.y || b.top)
+        viewportY: (b.y || b.top),
+        height: b.height
     }
+}
+
+function navBarPos(){
+    let imgSize = elementPosition(document.getElementById("hero_img"));
+    console.log(imgSize.height);
+    document.getElementById("main").style =
+        "    background-color: #4D4D4D; " +
+        "    position: absolute; " +
+        "    top: "+ imgSize +"px;";
+    return 1;
 }
 
 function keepMenuOnTop(){
     let posMenu = elementPosition(document.getElementById("menu"));
-    if  (document.getElementById("date").getBoundingClientRect().y >
-            document.getElementById("menu").getBoundingClientRect().height) {
+    if  (document.getElementById("date").getBoundingClientRect().y > posMenu.height + 40) {
         document.getElementById("menu").className = "row order-2 navbar navbar-dark bg-dark navbar-expand-lg menu";
-    }
-    else if (posMenu.viewportY < 0 || posMenu.viewportY === 0){
+        document.getElementById("date").className = "datepad";
+    }else if (posMenu.viewportY <= 0) {
         document.getElementById("menu").className = "row order-2 navbar navbar-dark bg-dark navbar-expand-lg menu2";
+        document.getElementById("date").className = "datepad2";
     }
 }
