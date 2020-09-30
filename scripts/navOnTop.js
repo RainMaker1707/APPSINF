04e9e19ae4880832bbcb0095a795c45cfca901d6
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2020-09-30 20:46:36 +0200

    translate main in french

--- scripts/navOnTop.js
+++ scripts/navOnTop.js
@@ -5,17 +5,18 @@ function elementPosition(a) {
         clientY: a.offsetTop,
         viewportX: (b.x || b.left),
         viewportY: (b.y || b.top),
-        height: b.height
+        height: b.height,
+        width: b.width
     }
 }
 
 function keepMenuOnTop(){
     let posMenu = elementPosition(document.getElementById("menu"));
-    if  (document.getElementById("date").getBoundingClientRect().y > posMenu.height + 1) {
-        document.getElementById("menu").className = "row order-2 navbar navbar-dark bg-dark navbar-expand-lg menu";
+    if  (document.getElementById("date").getBoundingClientRect().y > posMenu.height + .001) {
+        document.getElementById("menu").className = "row order-2 navbar navbar-expand-lg navbar-dark bg-dark menu";
         document.getElementById("date").className = "datepad";
     }else if (posMenu.viewportY <= 0) {
-        document.getElementById("menu").className = "row order-2 navbar navbar-dark bg-dark navbar-expand-lg fixed-top";
+        document.getElementById("menu").className = "row order-2 navbar navbar-expand-lg navbar-dark bg-dark menuFixed";
         document.getElementById("date").className = "datepad2";
     }
 }
@@ -23,6 +24,9 @@ function keepMenuOnTop(){
 function reposNavBar(){
     let mainContainer = document.getElementById("main");
     let heroImg = document.getElementById("hero_img");
-    let heroHeight = elementPosition(heroImg).height;
-    mainContainer.style.top = heroHeight + "px";
+    let heroSize = elementPosition(heroImg);
+    mainContainer.style.top = heroSize.height + "px";
+
+    let searchBar = document.getElementById("search");
+
 }
