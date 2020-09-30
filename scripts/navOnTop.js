2496ed11a8aa03c9a807790a4843f705c605cf13
Hadrien Allegaert <hadrien.allegaert@student.uclouvain.be>
2020-09-30 12:19:29 +0200

    first navbar responsive

--- scripts/navOnTop.js
+++ scripts/navOnTop.js
@@ -9,23 +9,13 @@ function elementPosition(a) {
     }
 }
 
-function navBarPos(){
-    let imgSize = elementPosition(document.getElementById("hero_img"));
-    console.log(imgSize.height);
-    document.getElementById("main").style =
-        "    background-color: #4D4D4D; " +
-        "    position: absolute; " +
-        "    top: "+ imgSize +"px;";
-    return 1;
-}
-
 function keepMenuOnTop(){
     let posMenu = elementPosition(document.getElementById("menu"));
-    if  (document.getElementById("date").getBoundingClientRect().y > posMenu.height + 40) {
+    if  (document.getElementById("date").getBoundingClientRect().y > posMenu.height + 1) {
         document.getElementById("menu").className = "row order-2 navbar navbar-dark bg-dark navbar-expand-lg menu";
         document.getElementById("date").className = "datepad";
     }else if (posMenu.viewportY <= 0) {
-        document.getElementById("menu").className = "row order-2 navbar navbar-dark bg-dark navbar-expand-lg menu2";
+        document.getElementById("menu").className = "row order-2 navbar navbar-dark bg-dark navbar-expand-lg fixed-top";
         document.getElementById("date").className = "datepad2";
     }
 }
